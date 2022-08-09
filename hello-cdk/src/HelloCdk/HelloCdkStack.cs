07e0c9b73664d50c998e933abc93c8babd025d1f
EC2 Default User <ec2-user@ip-172-31-60-111.ec2.internal>
2022-08-09 19:29:23 +0000

    working C# CDK

--- hello-cdk/src/HelloCdk/HelloCdkStack.cs
+++ hello-cdk/src/HelloCdk/HelloCdkStack.cs
@@ -5,9 +5,9 @@ namespace HelloCdk
 {
     public class HelloCdkStack : Stack
     {
-        public HelloCdkStack(Construct scope, string id, IStackProps props=null) : base(scope, id, props)
+        public HelloCdkStack(App scope, string id, IStackProps props=null) : base(scope, id, props)
         {
-            new Bucket(this, "MyFirstBucket08022022", new BucketProps
+            new Bucket(this, "random-rabbit-aug9", new BucketProps
             {
                 Versioned = true
             });
